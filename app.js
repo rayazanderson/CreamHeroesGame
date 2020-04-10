6b3567e82b62795dd09068d34a1e698aebe4a8a3
Raya Anderson <rayazand@gmail.com>
2020-04-09 22:38:23 -0700

    adding cards

---
+++ app.js
@@ -0,0 +1,44 @@
+document.addEventsListener('DOMContentLoaded', () => {
+
+
+    // card options here
+    const cardArray = [
+        {
+            name: "LaLa",
+            img: 'images/20190705_LaLa.jpg'
+        },
+        {
+            name: "ChuChu",
+            img: 'images/20190802_ChuChu.jpg'
+        },
+        {
+            name: "LuLu",
+            img: 'images/20190802_LuLu.jpg'
+        },
+        {
+            name: "TT",
+            img: 'images/20190802_TT.jpg'
+        },
+        {
+            name: "MoMo",
+            img: 'images/20190809_MoMo.jpg'
+        },
+        {
+            name: "DD",
+            img: 'images/20190816_DD.jpg'
+        },
+        {
+            name: "Coco",
+            img: 'images/20190828_creamheroes_coco.jpg'
+        },
+        {
+            name: "Lala",
+            img: 'images/20190705_LaLa.jpg'
+        },
+        {
+            name: "Lala",
+            img: 'images/20190705_LaLa.jpg'
+        },
+
+    ]
+})
