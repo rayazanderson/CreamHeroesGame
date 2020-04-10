88d26128af28100a3a245e4dd88d77cbba88010a
Raya Anderson <rayazand@gmail.com>
2020-04-09 22:52:03 -0700

    setting up the board

--- app.js
+++ app.js
@@ -8,8 +8,20 @@ document.addEventsListener('DOMContentLoaded', () => {
             img: 'images/20190705_LaLa.jpg'
         },
         {
+            name: "Lala",
+            img: 'images/20190705_LaLa.jpg'
+        },
+        {
             name: "ChuChu",
             img: 'images/20190802_ChuChu.jpg'
+        },
+        {
+            name: "ChuChu",
+            img: 'images/20190802_ChuChu.jpg'
+        },
+        {
+            name: "LuLu",
+            img: 'images/20190802_LuLu.jpg'
         },
         {
             name: "LuLu",
@@ -20,8 +32,20 @@ document.addEventsListener('DOMContentLoaded', () => {
             img: 'images/20190802_TT.jpg'
         },
         {
+            name: "TT",
+            img: 'images/20190802_TT.jpg'
+        },
+        {
             name: "MoMo",
             img: 'images/20190809_MoMo.jpg'
+        },
+        {
+            name: "MoMo",
+            img: 'images/20190809_MoMo.jpg'
+        },
+        {
+            name: "DD",
+            img: 'images/20190816_DD.jpg'
         },
         {
             name: "DD",
@@ -32,13 +56,21 @@ document.addEventsListener('DOMContentLoaded', () => {
             img: 'images/20190828_creamheroes_coco.jpg'
         },
         {
-            name: "Lala",
-            img: 'images/20190705_LaLa.jpg'
-        },
-        {
-            name: "Lala",
-            img: 'images/20190705_LaLa.jpg'
+            name: "Coco",
+            img: 'images/20190828_creamheroes_coco.jpg'
         },
 
     ]
+
+    const grid = document.querySelector.apply('.grid')
+    // Game board goes here
+    function createBoard() {
+        for (let i = 0; i < cardArray.length; i++) {
+            var card = document.createElement('img')
+            card.setAttribute('src', 'images/Blank-White-square-thumbnail.jpg')
+        }
+    }
+
+
+
 })
